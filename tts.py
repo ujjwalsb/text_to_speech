3a571d399afab374e6fd5bfd63613722a9e814b1
ujjwalsb <ujjwalsingh15@gmail.com>
2019-05-28 16:49:43 +0530

    [ADD] hindi translation

--- tts.py
+++ tts.py
@@ -7,7 +7,8 @@ from gtts import gTTS
 import os 
   
 # The text that you want to convert to audio 
-mytext = 'Welcome to the world of ujjwal singh baghel!'
+mytext = 'Welcome to the GitHub repository of ujjwal singh baghel!'
+mytext2 = 'ujjwal ke GitHub repository me aapka swagat hai'
   
 # Language in which you want to convert 
 language = 'en'
@@ -17,10 +18,13 @@ language = 'en'
 # the module that the converted audio should  
 # have a high speed 
 myobj = gTTS(text=mytext, lang=language, slow=False) 
+myobj2 = gTTS(text=mytext2, lang=language, slow=False) 
   
 # Saving the converted audio in a mp3 file named 
 # welcome  
 myobj.save("welcome.mp3") 
+myobj2.save("welcome_hindi.mp3") 
   
 # Playing the converted file 
 os.system("mpg321 welcome.mp3") 
+os.system("mpg321 welcome_hindi.mp3") 
